85deb95cedd54a0f9d2d14e12451bf07bf907b2b
mbright90 <michael.bright90@icloud.com>
2022-11-25 09:10:20 +0000

    Set up utility functions

--- src/domModules/domIndex.js
+++ src/domModules/domIndex.js
@@ -1,5 +1,21 @@
 const dom = (() => {
+  // Utility functions
+  function createClassElement(newElement, ...classes) {
+    const element = document.createElement(newElement)
+    classes.forEach((className) => element.classlist.add(className))
+    return element
+  }
 
+  HTMLElement.prototype.appendChildren = function appendChildren(...children) {
+    children.forEach((child) => this.appendChild(child))
+  }
+
+  // Parses an array of attributes where each attribute is a key/value object
+  HTMLElement.prototype.setAttributes = function setAttributes(...attributes) {
+    attributes.forEach((attribute) => )
+  }
+
+  // Initial page creation
 })()
 
 export default { dom }
