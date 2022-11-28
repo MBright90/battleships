991d06a58c71a9f957a3ab5c41abe25a8c10f879
mbright90 <michael.bright90@icloud.com>
2022-11-28 16:24:08 +0000

    Create initial page creation

--- src/domModules/domIndex.js
+++ src/domModules/domIndex.js
@@ -1,8 +1,16 @@
 const dom = (() => {
-  // Utility functions
+  // ***************** //
+  // Utility functions //
+  // ***************** //
   function createClassElement(newElement, ...classes) {
     const element = document.createElement(newElement)
     classes.forEach((className) => element.classlist.add(className))
+    return element
+  }
+
+  function createTextElement(newElement, text) {
+    const element = document.createElement(newElement)
+    element.textContent = text
     return element
   }
 
@@ -17,7 +25,36 @@ const dom = (() => {
     Object.keys(attributes).forEach((key) => this.setAttribute(key, attributes[key]))
   }
 
-  // Initial page creation
+  // ********************* //
+  // Initial page creation //
+  // ********************* //
+
+  const layoutWrapper = createClassElement('div', 'layout-wrapper')
+
+  // Create header
+  const header = document.createElement('header')
+  const headerTitle = createTextElement('h1', 'Battleships')
+  header.appendChild(headerTitle)
+
+  // Create main
+  const main = document.createElement('main')
+  main.appendChildren(
+    createClassElement('div', 'player-board'),
+    createClassElement('div', 'opponent-board'),
+  )
+
+  // Create footer
+  const footer = document.createElement('footer')
+  footer.appendChild(
+    createTextElement('p', 'MBright90'),
+  )
+
+  // Append all layout wrapper
+  layoutWrapper.appendChildren(
+    header,
+    main,
+    footer,
+  )
 })()
 
 export default { dom }
