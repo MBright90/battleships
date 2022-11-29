2fb527577360248725d2f2c6ec75f57b2d6d7a60
mbright90 <michael.bright90@icloud.com>
2022-11-29 15:05:13 +0000

    Add test for opponent initialization

--- tests/inititalTestCheck.test.js
+++ tests/inititalTestCheck.test.js
@@ -4,3 +4,8 @@ test('Umpire initializes score to zero', () => {
     const umpire = new Umpire()
     expect(umpire.score).toBe(0)
 })
+
+test('Umpire defaults to ai opponent when passed no value', () => {
+    const umpire = new Umpire()
+    expect(umpire.opponent).toBe('ai')
+})
