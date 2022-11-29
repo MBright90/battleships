7e6cba2347d3269e60548312ac1ae08e77a08633
mbright90 <michael.bright90@icloud.com>
2022-11-29 15:56:06 +0000

    Outlay skeleton functions

--- src/modules/umpire.js
+++ src/modules/umpire.js
@@ -3,6 +3,18 @@ class Umpire {
     this.score = 0
     this.opponent = opponent || 'ai'
   }
+
+  setUpGame() {
+    // Place logic for choosing ship spaces
+  }
+
+  playGame() {
+    // Place recursive logic for playing the game
+  }
+
+  resetGame() {
+    // Place logic for resetting the game
+  }
 }
 
 module.exports = Umpire
