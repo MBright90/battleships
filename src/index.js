f084b3d5d13826655f152ba53c735ce4dc905b1e
mbright90 <michael.bright90@icloud.com>
2022-12-01 10:09:24 +0000

    Add event listeners for game setup buttoins

--- src/index.js
+++ src/index.js
@@ -2,3 +2,27 @@ import './style/style.css'
 import dom from './modules/domIndex'
 
 document.body.appendChild(dom.initPage())
+
+// ****************** //
+// Listener functions //
+// ****************** //
+
+function gameSetupListeners() {
+  const buttons = document.querySelectorAll('.choices-container button')
+
+  buttons.forEach((button) => {
+    if (button.textContent === 'Player') {
+      button.addEventListener('click', () => {
+        console.log('clicked player')
+        buttons.forEach((choiceButton) => choiceButton.remove())
+      })
+    } else {
+      button.addEventListener('click', () => {
+        console.log('clicked ai')
+        buttons.forEach((choiceButton) => choiceButton.remove())
+      })
+    }
+  })
+}
+
+gameSetupListeners()
