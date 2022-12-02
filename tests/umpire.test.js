d29113971c1c982a021fae32ce7a4600acc6cafb
mbright90 <michael.bright90@icloud.com>
2022-12-02 10:07:26 +0000

    Add test for method of setOpponent

--- tests/umpire.test.js
+++ tests/umpire.test.js
@@ -9,3 +9,9 @@ test('Umpire defaults to ai opponent when passed no value', () => {
     const umpire = new Umpire()
     expect(umpire.opponent).toBe('ai')
 })
+
+test('Umpire changes opponent from default when selected', () => {
+    const umpire = new Umpire()
+    umpire.setOpponent('player')
+    expect(umpire.opponent).toBe('player')
+})
