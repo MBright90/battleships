6f7810b5747646ca8e8e29b35e0bce17ef364f67
mbright90 <michael.bright90@icloud.com>
2022-12-06 16:52:11 +0000

    Initiate utils module and transfer createCellArray function

---
+++ src/modules/utilities.js
@@ -0,0 +1,16 @@
+const utilities = (() => {
+  function createCellArray(cell, boardName, shipLength, axis) {
+    const cellArray = []
+    for (let i = 0; i < shipLength; i += 1) {
+      let currentCell
+      if (axis === 'x') currentCell = document.querySelector(`.${boardName} .cell[data-x-pos="${parseInt(cell.dataset.xPos, 10) + i}"][data-y-pos="${cell.dataset.yPos}"]`)
+      if (axis === 'y') currentCell = document.querySelector(`.${boardName} .cell[data-x-pos="${cell.dataset.xPos}"][data-y-pos="${parseInt(cell.dataset.yPos, 10) + i}"]`)
+      if (currentCell) cellArray[cellArray.length] = currentCell
+    }
+    return cellArray
+  }
+
+  return { createCellArray }
+})()
+
+export default utilities
