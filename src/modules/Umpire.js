5002d823147208a37170c0a5ff3bc00b5c44cca8
mbright90 <michael.bright90@icloud.com>
2022-12-06 16:06:22 +0000

    Add functions to get or change current player

--- src/modules/Umpire.js
+++ src/modules/Umpire.js
@@ -1,13 +1,24 @@
 class Umpire {
   constructor(opponent) {
     this.score = 0
+    this.currentPlayer = 'player-one'
     this.opponent = opponent || 'ai'
+
     this.playerMoves = []
     this.opponentMoves = []
   }
 
   setOpponent(opponent) {
     this.opponent = opponent
+  }
+
+  getCurrentPlayer() {
+    return this.currentPlayer
+  }
+
+  switchCurrentPlayer() {
+    if (this.currentPlayer === 'player-one') this.currentPlayer = 'player-two'
+    else this.currentPlayer = 'player-one'
   }
 
   setUpGame() {
