e0277b243e2f1c75f7873b8b51c80512aa60a2b6
mbright90 <michael.bright90@icloud.com>
2022-12-12 13:06:55 +0000

    Add tests for addShipPosition and allShipPositions

--- tests/player.test.js
+++ tests/player.test.js
@@ -2,5 +2,40 @@ const Player = require('../src/modules/Player')
 
 test('Returns correct image path for given ship', () => {
     const player = new Player()
-    expect(player.getImagePath('carrier')).toBe('./assets/images/carrier.jpg')
+    expect(player.getImagePath('carrier')).toBe('./assets/images/carrier.png')
+})
+
+test('Adds set of positions to given ship object', () => {
+    const player = new Player()
+    player.addShipPosition(['cell1', 'cell2', 'cell3', 'cell4', 'cell5'], 'carrier')
+    expect(player.ships.carrier.position).toStrictEqual(['cell1', 'cell2', 'cell3', 'cell4', 'cell5'])
+})
+
+test('returns each position of ships', () => {
+    const player = new Player()
+    player.ships = {
+        carrier: {
+            position: ['ca1', 'ca2', 'ca3', 'ca4', 'ca5'],
+        },
+        battleship: {
+            position: ['ba1', 'ba2', 'ba3', 'ba4'],
+        },
+        submarine: {
+            position: ['su1', 'su2', 'su3'],
+        },
+        cruiser: {
+            position: ['cr1', 'cr2', 'cr3'],
+        },
+        destroyer: {
+            position: ['de1', 'de2'],
+        },
+    }
+
+    expect(player.allShipPositions()).toBe([
+        'ca1', 'ca2', 'ca3', 'ca4', 'ca5', 
+        'ba1', 'ba2', 'ba3', 'ba4', 
+        'su1', 'su2', 'su3', 
+        'cr1', 'cr2', 'cr3', 
+        'de1', 'de2',
+    ])
 })
