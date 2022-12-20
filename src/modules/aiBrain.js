473f39601135fa3fa48a985002e80752d1174ca8
mbright90 <michael.bright90@icloud.com>
2022-12-20 19:52:08 +0000

    Add chooseShipPosition function

--- src/modules/aiBrain.js
+++ src/modules/aiBrain.js
@@ -4,7 +4,7 @@
 
 function generateRandom(maxNum) {
   // + 1 ensures number can include max and disregard 0
-  return Math.floor(Math.random() * maxNum)
+  return Math.floor(Math.random() * maxNum) + 1
 }
 
 function findCell(boardName, x, y) {
@@ -18,13 +18,19 @@ function findCell(boardName, x, y) {
 class Brain {
   constructor() {
     this.previousTurn = null
+    this.axisArray = ['x', 'y']
   }
 
   chooseSpace(board) {
-    const randomX = generateRandom(10) + 1
-    const randomY = generateRandom(10) + 1
-    console.log(randomX, randomY)
+    const randomX = generateRandom(10)
+    const randomY = generateRandom(10)
     return findCell(board, randomX, randomY)
+  }
+
+  chooseShipPosition(board) {
+    const cell = this.chooseSpace(board)
+    const randomAxis = this.axisArray[generateRandom(2)]
+    return [cell, randomAxis]
   }
 
   setPreviousTurn(cell) {
