5c307847bafe756d48240965c5579292752a8495
mbright90 <michael.bright90@icloud.com>
2022-12-26 13:32:43 +0000

    Refactor test to remove defunct tests

--- tests/umpire.test.js
+++ tests/umpire.test.js
@@ -4,9 +4,3 @@ test('Umpire initializes score to zero', () => {
     const umpire = new Umpire()
     expect(umpire.score).toBe(0)
 })
-
-test('Umpire changes opponent from default when selected', () => {
-    const umpire = new Umpire()
-    umpire.setOpponent('player')
-    expect(umpire.opponent).toBe('player')
-})
