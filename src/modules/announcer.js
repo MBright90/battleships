451beed047429eea67f1d2cc97823140d478f7d3
mbright90 <michael.bright90@icloud.com>
2023-01-08 16:53:06 +0000

    Init announcer file and skeleton methods

--- src/modules/announcer.js
+++ src/modules/announcer.js
@@ -1,3 +1,21 @@
-class Announcer {
-    
+// A class to handle the running commentary of the game, particularly to make
+// ai game play more immersive
+
+export default class Announcer {
+  constructor(container) {
+    this.container = container
+  }
+
+  announcePlacingShips(playerName) {
+    this.container.childNode.remove()
+
+  }
+
+  announceAiThinking(playerName) {
+
+  }
+
+  announceTurnOutcome(PlayerName) {
+
+  }
 }
